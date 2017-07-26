5080121adeae7ec530899b285031c330e5e0fbca
Tair Assimov <tair@assimov.net>
2017-07-26 16:33:32 +0200

    Fix bug with init of AccessControlLogService#logs

--- starter-code/src/app/services/access-control-log.service.ts
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AccessControlLogService {
 
-  logs: Array<Object> = [];
+  logs: Array<{person: string, message: string, createdAt: Date}> = [];
 
   constructor() { }
 
